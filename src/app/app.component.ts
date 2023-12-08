ebf281b5d34174578f67980ea8d61828b2d4f80a
Bruno Staine <brunostaine@hotmail.com>
2023-12-08 17:55:20 -0300

    Criação do footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
